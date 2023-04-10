05a50b09f5d1de3f1cba053a4860fa6de88e87ff
Pedrooozisqu <130400000+Pedrooozisqu@users.noreply.github.com>
2023-04-10 18:19:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+//var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area  =  altura * comprimento;
+//console.log(area);
+//ar area;
+
+// let 
+
+
+//let altura = 5
+//let comprimento = 7
+
+//let forma = 'retangulo'
+
+//if(forma === 'retângulo'){
+    //area = altura * comprimento}
+//else{
+
+    area= (altura * comprimento)/2
+//}//
+
+//console.log(area)
+
+
+//const 
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area = altura * comprimento
+ 
+
+
+if (forma === 'quadrado'){
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+console.log(area)
